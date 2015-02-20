9ed3d0e329c60ae02083d3acafc7bcf7c068cf0c
Zsolt Gál <zsolt.gal@sessiondigital.com>
2015-02-20 14:55:01 +0100

    Fix what-did-i-work-on-recently to report current users activity instead of hard-coded username

--- plugins-available/abbreviations.fish
+++ plugins-available/abbreviations.fish
@@ -1,16 +1,16 @@
 alias lh="ls -hal"
 alias grep="grep --color=auto"
-alias findf="find . -type f"
-alias findd="find . -type d"
-alias findphp="find . -type f -name \"*.php\""
-alias findinphp='find . -type f -name "*.php" | xargs grep --color=auto -n'
-alias findphtml="find . -type f -name \"*.phtml\" | xargs grep --color=auto -n"
+alias findf="find . -type f | xargs sed 's/^\.\///g;s/: / :/g'"
+alias findd="find . -type d | xargs sed 's/^\.\///g;s/: / :/g'"
+alias findphp="find . -type f -name \"*.php\" | sed 's/^\.\///g;s/: / :/g' | xargs grep --color=auto -n"
+alias findinphp="find . -type f -name \"*.php\" | sed 's/^\.\///g;s/: / :/g' | xargs grep --color=auto -n"
+alias findphtml="find . -type f -name \"*.phtml\" | sed 's/^\.\///g;s/: / :/g' | xargs grep --color=auto -n"
 alias findxml="find . -type f -name \"*.xml\" | xargs grep --color=auto -n"
-alias what-did-i-work-on-recently='git log --format="%ae %ad %s" --date=short -100 | grep "zsolt.gal@sessiondigital.com" | grep -oE "[0-9\-]+ [A-Z]+-[0-9]+.*"'
+# alias what-did-i-work-on-recently='git log --format="%ae %ad %s" --date=short -100 | grep "$__git_email" | grep -oE "[0-9\-]+ [A-Z]+-[0-9]+.*"'
 alias runningvms='VBoxManage list runningvms | cut -d " " -f 1'
 alias stopallvms='VBoxManage list runningvms | cut -d " " -f 1 | xargs -J % VBoxManage controlvm % savestate'
-alias pull="git pull"
-alias push="git push origin head"
-alias merge="git merge"
-alias got="git"
-alias go="git checkout"
+function what-did-i-work-on-recently --description "List branches you were working on recently"
+  set --local --unexport __git_email (git config --get-all user.email | tail -1)
+  echo $__git_email worklog:
+  git log --format="%ae %ad %s" --date=short -100 | grep $__git_email | grep -oE "[0-9\-]+ [A-Z]+-[0-9]+.*"
+end
