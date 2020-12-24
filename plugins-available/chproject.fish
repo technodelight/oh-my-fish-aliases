fdadd08bf2e58f19d97beba20bdcb5ae4ee4e123
Zsolt Gál <zsolt.gal@sessiondigital.com>
2020-12-24 13:21:27 +0100

    do not load chproject on non osx envs

--- plugins-available/chproject.fish
+++ plugins-available/chproject.fish
@@ -1,19 +1,21 @@
 set __projectname_basedir /Users/galzsolt/Sites /Users/galzsolt/MySites
 set __projectname_sed_regex 's/\.development\.local//;s/dev\.//;s/\.com//'
 
-# change to a project directory w/ autocomplete
-function project --description 'Change to project directory'
-    set --unexport --local tmp_dir (find $__projectname_basedir -type d -maxdepth 1 -mindepth 1 | grep $argv)
-    if test -d "$tmp_dir"
-        cd "$tmp_dir"
-    else
-        echo -n 'No such project ('
-        echo -n $argv
-        echo ')'
+if test (uname) != "Linux"
+    # change to a project directory w/ autocomplete
+    function project --description 'Change to project directory'
+        set --unexport --local tmp_dir (find $__projectname_basedir -type d -maxdepth 1 -mindepth 1 | grep $argv)
+        if test -d "$tmp_dir"
+            cd "$tmp_dir"
+        else
+            echo -n 'No such project ('
+            echo -n $argv
+            echo ')'
+        end
     end
-end
 
-complete -f -c project -e
-for projectname in (find $__projectname_basedir -type d -maxdepth 1 -mindepth 1 | sed $__projectname_sed_regex | xargs basename)
-    complete -f -c project -A -a $projectname -d "Change to $projectname directory"
+    complete -f -c project -e
+    for projectname in (find $__projectname_basedir -type d -maxdepth 1 -mindepth 1 | sed $__projectname_sed_regex | xargs basename)
+        complete -f -c project -A -a $projectname -d "Change to $projectname directory"
+    end
 end
